709f01c33e7d5b448d4e6841ba8b26d5615c90a8
AnilBamne <anilbamne20@gmail.com>
2022-12-14 23:26:24 +0530

    Adding:Welcome Message

---
+++ PersonDataManagement222Batch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonDataManagement222Batch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to PERSON DATA MANAGEMENT program");
+        }
+    }
+}

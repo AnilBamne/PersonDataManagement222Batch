7f7e378f2e3b92fce0fd17a149baba802553c90e
AnilBamne <anilbamne20@gmail.com>
2022-12-14 23:47:16 +0530

    Adding:UC1CreatingListOfPersons code

---
+++ PersonDataManagement222Batch/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDataManagement222Batch
+{
+    /// <summary>
+    /// Creating a Person class
+    /// encapsulating the person details
+    /// overriding the toString method
+    /// </summary>
+    public class Person
+    {
+        public int SSN { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return $"SerialNo:{SSN} Name:{Name} Age:{Age} Address:{Address}";
+        }
+    }
+}
--- PersonDataManagement222Batch/Program.cs
+++ PersonDataManagement222Batch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PersonDataManagement222Batch
 {
@@ -7,6 +8,34 @@ namespace PersonDataManagement222Batch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to PERSON DATA MANAGEMENT program");
+            List<Person> list = new List<Person>();
+            AddingPersonDetailsToList(list);
+            DisplayPersonDetails(list);
+        }
+        /// <summary>
+        /// Adding the details of each person to the list
+        /// </summary>
+        /// <param name="list"></param>
+        public static void AddingPersonDetailsToList(List<Person> list)
+        {
+            list.Add(new Person() { SSN = 1, Name = "Anil", Address = "Bidar", Age = 12, });
+            list.Add(new Person() { SSN = 2, Name = "Sunil", Address = "Pune", Age = 70, });
+            list.Add(new Person() { SSN = 3, Name = "Shubhad", Address = "Mumbai", Age = 32, });
+            list.Add(new Person() { SSN = 4, Name = "Anish", Address = "Hydrabad", Age = 16, });
+            list.Add(new Person() { SSN = 5, Name = "Vinay", Address = "Mumbai", Age = 63, });
+            list.Add(new Person() { SSN = 6, Name = "Kiran", Address = "Chennai", Age = 36, });
+            list.Add(new Person() { SSN = 7, Name = "Akshata", Address = "Banglore", Age = 14, });
+        }
+        /// <summary>
+        /// Displaying person details
+        /// </summary>
+        /// <param name="list"></param>
+        public static void DisplayPersonDetails(List<Person> list)
+        {
+            foreach (Person person in list)
+            {
+                Console.WriteLine(person);
+            }
         }
     }
 }
